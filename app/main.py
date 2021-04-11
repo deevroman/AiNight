3937effb1fb7bb9aaa90d19a6b34a4d1c46cd7a9
deevroman <roman.deev06@gmail.com>
2021-04-11 17:09:45 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
 from flask_cors import CORS, cross_origin
 import psycopg2
@@ -8,26 +8,42 @@ import cv2
 import face_recognition
 import os
 import random
+import imghdr
 
 host = os.environ.get('STORAGE_HOST', None)
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 connection_db = psycopg2.connect("user='aires4' password='aires4' host='{}' dbname='postgres'".format(host))
 
 
+class FaceNotFound(Exception):
+    pass
+
+
+def check_photo(file_name):
+    if imghdr.what(file_name) is None:
+        raise Exception("Bad photo")
+
+
 def save_img(file_name, name):
+    check_photo(file_name)
+    try:
+        image = cv2.imread(file_name)
+    except cv2.error as e:
+        raise Exception('Bad photo')
     face_detector = dlib.get_frontal_face_detector()
-    image = cv2.imread(file_name)
     detected_faces = face_detector(image, 1)
     print("Found {} faces in the image file {}".format(len(detected_faces), file_name))
+    if len(detected_faces) == 0:
+        raise FaceNotFound("На фото нет лица")
     for i, face_rect in enumerate(detected_faces):
         print("- Face #{} found at Left: {} Top: {} Right: {} Bottom: {}".format(i, face_rect.left(), face_rect.top(),
                                                                                  face_rect.right(), face_rect.bottom()))
         crop = image[face_rect.top():face_rect.bottom(), face_rect.left():face_rect.right()]
         encodings = face_recognition.face_encodings(crop)
-        print(encodings)
+        # print(encodings)
         if len(encodings) > 0:
             db = connection_db.cursor()
             query = "SELECT count(*) FROM vectors WHERE file='{}'".format(name)
@@ -42,16 +58,22 @@ def save_img(file_name, name):
             db.execute(query)
             # print(query)
             connection_db.commit()
+            db.close()
         else:
-            raise Exception("На фото нет лица")
+            raise FaceNotFound("На фото нет лица")
 
 
 def find_face(file_name):
+    check_photo(file_name)
+    try:
+        image = cv2.imread(file_name)
+    except cv2.error as e:
+        raise Exception('Bad photo')
     face_detector = dlib.get_frontal_face_detector()
-    image = cv2.imread(file_name)
     detected_faces = face_detector(image, 1)
-
     print("Found {} faces in the image file {}".format(len(detected_faces), file_name))
+    if len(detected_faces) == 0:
+        raise FaceNotFound("На фото нет лица")
     for i, face_rect in enumerate(detected_faces):
         print("- Face #{} found at Left: {} Top: {} Right: {} Bottom: {}".format(i, face_rect.left(), face_rect.top(),
                                                                                  face_rect.right(), face_rect.bottom()))
@@ -72,19 +94,21 @@ def find_face(file_name):
 
             while row is not None:
                 print(row)
+                db.close()
                 return row[0]
-                row = db.fetchone()
-
-        #             db.close()
+                # row = db.fetchone()
+            raise FaceNotFound("Не найдено похожих лиц")
         else:
-            raise Exception("На фото нет лица")
+            raise FaceNotFound("На фото нет лица")
 
 
 def get_db_size():
     db = connection_db.cursor()
-    query = "SELECT count(*)"
+    query = "SELECT count(*) from vectors"
     db.execute(query)
-    return db.fetchone()[0]
+    row = db.fetchone()
+    db.close()
+    return row[0]
 
 
 @app.route('/get_count_faces', methods=['GET'])
@@ -100,8 +124,10 @@ def get_find_face():
     request.files['image'].save(path)
     try:
         return find_face(path), 200
-    except:
-        return "bad", 200
+    except FaceNotFound as e:
+        return str(e) + "</br>Количество лиц  базе: " + str(get_db_size()), 200
+    except Exception as e:
+        return str(e), 200
 
 
 @app.route('/add_face', methods=['POST'])
@@ -109,9 +135,11 @@ def get_find_face():
 def post_save_face():
     name = request.form['name']
     path = "app/tmp/" + name
+    if len(name) > 255:
+        return "Long name", 200
     for i in name:
         if i.lower() not in 'abcdefghijklmnopqrstuvwxyz0123456789_абвгдеёжзийклмнопрстуфхцчшщъыьэюя':
-            return "Invalid path", 200
+            return "Invalid name", 200
     request.files['image'].save(path)
     try:
         save_img(path, name)
@@ -124,41 +152,61 @@ def post_save_face():
 
 
 def init():
-    try:
-        save_img("app/photos/navalny.jpg", "navalny")
-        save_img("app/photos/putin.jpg", "putin")
-        save_img("app/photos/solovei.jpg", "solovei")
-        save_img("app/photos/zelen.jpg", "zelen")
-    except Exception as e:
-        return str(e), 200
-    return "Сброшено", 200
+    save_img("app/photos/navalny.jpg", "navalny")
+    save_img("app/photos/putin.jpg", "putin")
+    save_img("app/photos/solovei.jpg", "solovei")
+    save_img("app/photos/zelen.jpg", "zelen")
 
 
 @app.route('/reset', methods=['GET'])
 @cross_origin()
 def reset():
-    db = connection_db.cursor()
-    query = "TRUNCATE vectors"
-    db.execute(query)
-    connection_db.commit()
-    init()
+    try:
+        db = connection_db.cursor()
+        query = "TRUNCATE vectors"
+        db.execute(query)
+        connection_db.commit()
+        init()
+    except Exception as e:
+        print(e)
+        return "Не удалось", 200
     return "Сброшено", 200
+
+
+@app.route('/print_db', methods=['GET'])
+@cross_origin()
+def print_db():
+    db = connection_db.cursor()
+    query = "SELECT file from vectors"
+    db.execute(query)
+    ans = ""
+    row = db.fetchone()
+    while row is not None:
+        ans += row[0] + "</br>"
+        row = db.fetchone()
+    db.close()
+    return ans, 200
 
 
 @app.route('/test/<int:pic_id>', methods=['GET'])
 @cross_origin()
 def test(pic_id):
-    if pic_id == 1:
-        return find_face("app/test/navalny.jpg")
-    elif pic_id == 2:
-        return find_face("app/test/putin.jpg")
-    elif pic_id == 3:
-        return find_face("app/test/solovei.jpg")
-    elif pic_id == 4:
-        return find_face("app/test/zelen.jpg")
+    try:
+        if pic_id == 1:
+            return find_face("app/test/navalny.jpg")
+        elif pic_id == 2:
+            return find_face("app/test/solovei.jpg")
+        elif pic_id == 3:
+            return find_face("app/test/putin.jpg")
+        elif pic_id == 4:
+            return find_face("app/test/zelen.jpg")
+    except FaceNotFound as e:
+        return str(e) + "</br>Количество лиц  базе: " + str(get_db_size()), 200
+    except Exception as e:
+        return str(e), 200
     return "", 200
 
 
 @app.route('/')
 def index():
-    return "<h1>KEK</h1>"
+    return render_template('index.html')
